f91e38802f8ac33bf50a2e225df2b499ea6e76ea
chengyipeng <yip.cheng001@bkjk.com>
2019-09-04 22:58:32 +0800

    策略模式代码push

--- strategy-pattern/src/main/java/com/peng/pay/PayTest.java
+++ strategy-pattern/src/main/java/com/peng/pay/PayTest.java
@@ -5,5 +5,6 @@ public class PayTest {
 	public static void main(String[] args) {
 		Order order = new  Order("2121", "21322121", 700);
 		PayManager.pay(PayManager.PayType.WECHANTPAY.toString(), order);
+		
 	}
 }
